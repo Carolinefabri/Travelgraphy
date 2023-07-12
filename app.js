ae7d3e37b38b0f243b0aac6d5ff4b8edcb4b285e
“Caroline <carolinefabri.dev@hotmail.com>
2023-07-12 18:18:42 +0200

    test

--- app.js
+++ app.js
@@ -28,3 +28,11 @@ app.use('/', indexRoutes)
 require('./travelgraphy/error-handling')(app)
 
 module.exports = app
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on :${PORT}`);
+});
